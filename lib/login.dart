eefd199c5b8144fe13b2c7dbbbd51350e2a54355
Prateek <2018csb1114@iitrpr.ac.in>
2021-05-07 00:06:02 +0530

    Done

---
+++ lib/login.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:phone_auth_project/otp.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(children: [
+            Container(
+              margin: EdgeInsets.only(top: 60),
+              child: Center(
+                child: Text(
+                  'Enter Mobile Number',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 60),
+              child: Center(
+                child: Text(
+                  'You will recieve a 6 digit OTP',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 40, right: 10, left: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Phone Number',
+                  prefix: Padding(
+                    padding: EdgeInsets.all(4),
+                    child: Text('+91'),
+                  ),
+                ),
+                maxLength: 10,
+                keyboardType: TextInputType.number,
+                controller: _controller,
+              ),
+            )
+          ]),
+          Container(
+            margin: EdgeInsets.all(10),
+            width: double.infinity,
+            child: FlatButton(
+              color: Colors.blue,
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => OTPScreen(_controller.text)));
+              },
+              child: Text(
+                'Next',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
